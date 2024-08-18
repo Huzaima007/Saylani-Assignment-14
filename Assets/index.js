ecf30e3d8db56a3d0e6dfe35a6970eaf27052c5b
Huzaima007 <huzaimasiddiq17@gmail.com>
2024-08-18 13:55:32 +0500

    Create index.js

---
+++ Assets/index.js
@@ -0,0 +1,11 @@
+var array = [1,2,3,4,5];
+var removevalue = array.pop();
+console.log(array,removevalue);
+
+
+
+var array = [1,2,3,4,5];
+var removevalue = array.push(8);
+console.log(array,removevalue);
+
+
